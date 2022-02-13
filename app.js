2d6a9d991b741a26945ef713b96a9fc560f163f7
raych2 <rtongco@gmail.com>
2022-02-12 20:40:09 -0800

    Set up and connect to MongoDB

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ const bcrypt = require("bcryptjs");
 
 const indexRouter = require("./routes/index");
 
+const mongoDB = process.env.MONGO_URI;
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 const app = express();
 
 app.use(logger("dev"));
