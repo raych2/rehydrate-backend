1b2e4ebba99b93d409aec0ad0da2e1bbe307e5c4
raych2 <rtongco@gmail.com>
2022-02-14 20:23:04 -0800

    Add cors to app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const indexRouter = require("./routes/index");
 
@@ -22,6 +23,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
